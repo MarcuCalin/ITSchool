756aa5183eaf0198d4a96ecd151367575e5ba588
Calin Marcu <marcuviorelcalin@gmail.com>
2025-06-14 15:35:59 +0300

    Python ex

---
+++ Python/ex2-variables.py
@@ -0,0 +1,7 @@
+age = 16
+cost = 12.5
+name = "Calin Marcu"
+is_valid = True
+
+print(f"Cost is {cost} age is {age} and name is {name} and is valid {is_valid}")
+print(f"E major: {age >= 18}")
---
+++ Python/ex3-variables-print.py
@@ -0,0 +1,13 @@
+age = 16
+cost = 12.5
+name = "Calin Marcu"
+is_valid = True
+
+print("Age is " , age)
+print("Age is " +str(age))
+print(f"Age is {age}")
+
+mesaj = "Salut , " + name + " cu varsta de " + str(age) 
+print(mesaj)
+
+# complex_message = f"""
